c13467570c663beee7d96cf2a5cdf9a91649d7dd
Patrick Nobbe <patrick.nobbe@hotmail.com>
2020-06-30 15:59:54 +0200

    Update main page title

--- src/router/index.js
+++ src/router/index.js
@@ -4,29 +4,39 @@ import HomePage from '../views/HomePage.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{
-    path: '/',
-    name: 'HomePage',
-    component: HomePage
-  },
-  {
-    path: '/404',
-    name: '404',
-    component: () => import('../views/404.vue')
-  },
-  {
-    path: '/faq',
-    name: 'FaqPage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/FaqPage.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'HomePage',
+        component: HomePage,
+        meta: {
+            title: "FunderMaps",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "The home page of FunderMaps B.V."
+                }
+            ]
+        }
+    },
+    {
+        path: '/404',
+        name: '404',
+        component: () => import('../views/404.vue')
+    },
+    {
+        path: '/faq',
+        name: 'FaqPage',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../views/FaqPage.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes
 })
 
 
